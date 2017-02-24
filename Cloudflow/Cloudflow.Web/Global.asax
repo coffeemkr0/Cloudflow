308408393d73827c870b6d8c950528ee90a085a9
coffeemkr0 <mike.e.coffey@gmail.com>
2017-02-24 16:44:28 -0600

    Create solution and add initial projects.

---
+++ Cloudflow/Cloudflow.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cloudflow.Web.MvcApplication" Language="C#" %>
---
+++ Cloudflow/Cloudflow.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cloudflow.Web.Startup))]
+namespace Cloudflow.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
