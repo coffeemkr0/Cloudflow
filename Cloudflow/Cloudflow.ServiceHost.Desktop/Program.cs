ac8fdb823a47b55bd568f28a0d8b8fedb773bead
coffeemkr0 <mike.e.coffey@gmail.com>
2017-02-24 21:16:18 -0600

    Host the service in the console app.

--- Cloudflow/Cloudflow.ServiceHost.Desktop/Program.cs
+++ Cloudflow/Cloudflow.ServiceHost.Desktop/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Web;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace Cloudflow.ServiceHost.Desktop
     {
         static void Main(string[] args)
         {
+            //Requires this admin level command on PC "netsh http add urlacl url=http://+:80/Service1 user=domain\user"
+            using (var host = new WebServiceHost(typeof(Cloudflow.WcfServiceLibrary.Service1)))
+            {
+                // Start listening for messages
+                host.Open();
+
+                Console.WriteLine("Press any key to stop the service.");
+                Console.ReadKey();
+
+                // Close the service
+                host.Close();
+            }
         }
     }
 }
