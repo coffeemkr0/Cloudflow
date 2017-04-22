8dd385fa67f31b76c49b002326cebce4194af216
coffeemkr0 <mike.e.coffey@gmail.com>
2017-04-22 09:29:47 -0500

    Remove the default About and Contact Actions and Views from the project - these were added by the project wizard when it was created by visual studio.

--- Cloudflow/Cloudflow.Web/Controllers/HomeController.cs
+++ Cloudflow/Cloudflow.Web/Controllers/HomeController.cs
@@ -28,20 +28,6 @@ namespace Cloudflow.Web.Controllers
             return View(model);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (_databaseContext != null)
