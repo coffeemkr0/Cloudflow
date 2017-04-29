718f60beba7993256e1b4c0a164b8bcc14437dac
coffeemkr0 <mike.e.coffey@gmail.com>
2017-04-29 11:03:28 -0500

    Add a simple hub to the agent service project.

---
+++ Cloudflow/Cloudflow.Agent.Service/Hubs/GeneralMessageHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloudflow.Agent.Service.Hubs
+{
+    /// <summary>
+    /// A SignalR hub for sending simple string messages.
+    /// </summary>
+    public class GeneralMessageHub : Hub
+    {
+        #region Public Methods
+        /// <summary>
+        /// Sends a string message to all connected clients.
+        /// </summary>
+        /// <param name="message">The message to send to the client</param>
+        public void Send(string message)
+        {
+            Clients.All.sendMessage(message);
+        }
+        #endregion
+    }
+}
