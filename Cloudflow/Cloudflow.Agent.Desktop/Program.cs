dd1ed3f45cc0bdf44983524564bbee4d368d5143
coffeemkr0 <mike.e.coffey@gmail.com>
2017-04-29 17:56:22 -0500

    Add exception handling to the desktop agent.

--- Cloudflow/Cloudflow.Agent.Desktop/Program.cs
+++ Cloudflow/Cloudflow.Agent.Desktop/Program.cs
@@ -21,19 +21,20 @@ namespace Cloudflow.Agent.Desktop
             //Load hubs from the Agent.Service assembly so that SignalR will pick them up
             AppDomain.CurrentDomain.Load(typeof(GeneralMessageHub).Assembly.FullName);
 
-            //Setup the SignalR messaging service first so that we can let clients know what is going on
-            string url = "http://+:80/CloudflowMessaging/";
-            var signalRHost = WebApp.Start<SignalRStartup>(url);
-            log.Info(string.Format("Cloudflow messaging service started and running on {0}", url));
-
-            //Setup the WCF service that does all the real work
-            //Requires this admin level command on the PC that will host the service:
-            //"netsh http add urlacl url=http://+:80/ServiceName user=domain\user"
-            var epAddress = "http://localhost/CloudflowAgentService";
-            Uri[] baseAddresses = new Uri[] { new Uri(epAddress) };
-            using (var wcfHost = new CorsEnabledServiceHost(typeof(AgentService), 
-                baseAddresses))
+            try
             {
+                //Setup the SignalR messaging service first so that we can let clients know what is going on
+                string url = "http://+:80/CloudflowMessaging/";
+                var signalRHost = WebApp.Start<SignalRStartup>(url);
+                log.Info(string.Format("Cloudflow messaging service started and running on {0}", url));
+
+                //Setup the WCF service that does all the real work
+                //Requires this admin level command on the PC that will host the service:
+                //"netsh http add urlacl url=http://+:80/ServiceName user=domain\user"
+                var epAddress = "http://localhost/CloudflowAgentService";
+                Uri[] baseAddresses = new Uri[] { new Uri(epAddress) };
+                var wcfHost = new CorsEnabledServiceHost(typeof(AgentService), baseAddresses);
+
                 // Start listening for messages
                 wcfHost.Open();
 
@@ -41,14 +42,30 @@ namespace Cloudflow.Agent.Desktop
 
                 Console.WriteLine("Press Ctrl+C or close this window to stop the services.");
                 var result = Console.ReadKey();
-                while(result.Modifiers != ConsoleModifiers.Control && result.Key != ConsoleKey.C)
+                while (result.Modifiers != ConsoleModifiers.Control && result.Key != ConsoleKey.C)
                 {
                     result = Console.ReadKey();
                 }
 
-                // Close the hosts
                 signalRHost.Dispose();
                 wcfHost.Close();
+            }
+            catch (System.Reflection.TargetInvocationException targetInvocationEx)
+            {
+                log.Warn("Could not start the agent. Make sure that the Cloudflow.Agent.Setup program has been used to register the needed urls.");
+                log.Error(targetInvocationEx);
+                Console.ReadKey();
+            }
+            catch (System.ServiceModel.AddressAccessDeniedException addressDeniedEx)
+            {
+                log.Warn("Could not start the agent. Make sure that the Cloudflow.Agent.Setup program has been used to register the needed urls.");
+                log.Error(addressDeniedEx);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                log.Fatal(ex);
+                Console.ReadKey();
             }
         }
     }
