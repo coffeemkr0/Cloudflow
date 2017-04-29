fb12f0209640710bbe5b3fb588f4812e5c046803
coffeemkr0 <mike.e.coffey@gmail.com>
2017-04-29 10:58:17 -0500

    Add the Owin.Cors nuget package so that we can enable Cors on the SignalR service. Also add the SignalR startup class that holds the configuration options for SignalR.

---
+++ Cloudflow/Cloudflow.Agent.Desktop/SignalRStartup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloudflow.Agent.Desktop
+{
+    public class SignalRStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+
+                var hubConfifuration = new HubConfiguration
+                {
+                    EnableDetailedErrors = true,
+                    EnableJSONP = true
+                };
+
+                map.RunSignalR(hubConfifuration);
+            });
+        }
+    }
+}
