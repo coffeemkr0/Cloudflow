9987ffd0b927db80f7198b8c5245c0a5db9691e9
coffeemkr0 <mike.e.coffey@gmail.com>
2017-05-01 21:35:14 -0500

    Move javascript to files and fix bad merge that overwrote the fix for handling multiple agents on different PCs.

---
+++ Cloudflow/Cloudflow.Web/Scripts/Home/Index.js
@@ -0,0 +1,42 @@
+﻿
+function HomeIndex() {
+    this.MachineNames = null;
+}
+
+$(function () {
+    AgentControllerClient.AgentConnected = AgentConnected;
+    AgentControllerClient.AgentMessageReceived = AgentMessageReceived;
+
+    AgentControllerClient.ConnectToAgents(HomeIndex.MachineNames);
+
+    $(".startAgentLink").each(function () {
+        $(this).on("click", function (e) {
+            var $item = $(e.target);
+            var machineName = $item.attr("data-machinename");
+            AgentControllerClient.StartAgent(machineName, function () {
+                SetStatusText(machineName);
+            });
+            return false;
+        });
+    });
+});
+
+function AgentConnected(machineName) {
+    SetStatusText(machineName);
+}
+
+function AgentMessageReceived(machinName, message) {
+    console.log("Message received from " + machineName + " - " + message);
+}
+
+function StartAgent(machineName) {
+    AgentControllerClient.StartAgent(machineName, function () {
+        SetStatusText(machineName);
+    });
+}
+
+function SetStatusText(machineName) {
+    AgentControllerClient.GetAgentStatus(machineName, function (status) {
+        $("#agentStatus-" + machineName).text(status);
+    });
+}
---
+++ Cloudflow/Cloudflow.Web/Scripts/Shared/AgentControllerClient.js
@@ -0,0 +1,78 @@
+﻿
+function AgentControllerClient() {
+    this.AgentControllerProxies = null;
+    this.AgentMessageReceived = null
+    this.AgentConnected = null;
+}
+
+AgentControllerClient.ConnectToAgents = function (machineNames) {
+
+    AgentControllerClient.AgentControllerProxies = [];
+
+    machineNames.forEach(function (machineName) {
+        //Create a connetion to the agent
+        var connection = $.hubConnection("http://" + machineName + ":80/CloudflowAgent/signalr");
+
+        //For each hub we care about, create a proxy and sign up for the methods that the server will call to the client
+        var agentControllerProxy = connection.createHubProxy("agentController");
+        agentControllerProxy.on('addMessage', function (message) {
+            console.log("Message from " + machinName + " - " + message);
+        });
+
+        //Open the connection
+        connection.start().done(function () {
+            //If the connection was successful, store the proxy so we can reuse it
+            AgentControllerClient.AgentControllerProxies.push({ machineName: machineName, proxy: agentControllerProxy });
+            //Let subscribers know that the agent is now connected
+            AgentControllerClient.AgentConnected(machineName);
+        }).fail(function () {
+            console.log("Could not connect to " + machineName); 
+        });
+    });
+};
+
+AgentControllerClient.StartAgent = function (machineName, callback) {
+    var agentControllerProxyEntry = AgentControllerClient.AgentControllerProxies.find(function (item) {
+        return item.machineName === machineName;
+    });
+
+    if (typeof agentControllerProxyEntry !== "undefined") {
+        agentControllerProxyEntry.proxy.invoke("startAgent").done(function () {
+            callback();
+        }).fail(function () {
+            callback();
+        });
+    }
+};
+
+AgentControllerClient.StopAgent = function (machineName, callback) {
+    var agentControllerProxyEntry = AgentControllerClient.AgentControllerProxies.find(function (item) {
+        return item.machineName === machineName;
+    });
+
+    if (typeof agentControllerProxyEntry !== "undefined") {
+        agentControllerProxyEntry.proxy.invoke("stopAgent").done(function () {
+            callback();
+        }).fail(function () {
+            callback();
+        });
+    }
+};
+
+AgentControllerClient.GetAgentStatus = function (machineName, callback) {
+    var agentControllerProxyEntry = AgentControllerClient.AgentControllerProxies.find(function (item) {
+        return item.machineName === machineName;
+    });
+
+    if (typeof agentControllerProxyEntry !== "undefined") {
+        agentControllerProxyEntry.proxy.invoke("getAgentStatus").done(function (status) {
+            callback(status.StatusDisplayText);
+        }).fail(function (error) {
+            callback("Get Agent Status Failed");
+        });
+    }
+};
+
+AgentControllerClient.AddMessage = function (machineName, message) {
+    this.AgentMessageReceived(machineName, message);
+};
