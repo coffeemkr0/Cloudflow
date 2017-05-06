10238869ee7ef814dbfe33b5fb87b28fa533affb
coffeemkr0 <mike.e.coffey@gmail.com>
2017-05-05 20:52:23 -0500

    Add a controller and view for viewing agents.

---
+++ Cloudflow/Cloudflow.Web/Controllers/AgentsController.cs
@@ -0,0 +1,35 @@
+﻿using Cloudflow.Core.Data;
+using Cloudflow.Web.ViewModels.Agents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cloudflow.Web.Controllers
+{
+    public class AgentsController : Controller
+    {
+        #region Module Level Declarations
+        CoreDbContext _databaseContext;
+        #endregion
+
+        #region Actions
+        // GET: Agents
+        public ActionResult Index()
+        {
+#if DEBUG
+            _databaseContext = new CoreDbContext(true);
+#else
+            _databaseContext = new CoreDbContext();
+#endif
+
+            IndexViewModel model = new IndexViewModel();
+
+            model.AgentConfigurations.AddRange(_databaseContext.AgentConfigurations.ToList());
+
+            return View(model);
+        }
+        #endregion
+    }
+}
---
+++ Cloudflow/Cloudflow.Web/ViewModels/Agents/IndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Cloudflow.Core.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cloudflow.Web.ViewModels.Agents
+{
+    public class IndexViewModel
+    {
+        #region Properties
+        public List<AgentConfiguration> AgentConfigurations { get; set; }
+        #endregion
+
+        #region Constructors
+        public IndexViewModel()
+        {
+            this.AgentConfigurations = new List<AgentConfiguration>();
+        }
+        #endregion
+    }
+}
