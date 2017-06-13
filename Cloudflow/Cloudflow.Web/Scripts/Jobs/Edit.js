56df87e38125ce43fdbfbe834894f0f6c4375ab2
coffeemkr0 <mike.e.coffey@gmail.com>
2017-06-12 20:04:18 -0500

    Add buttons for adding triggers and steps and stub out javascript event handlers for their click events.

---
+++ Cloudflow/Cloudflow.Web/Scripts/Jobs/Edit.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $("#btnAddTrigger").on("click", btnAddTrigger_Clicked);
+    $("#btnAddStep").on("click", btnAddStep_Clicked);
+});
+
+function btnAddTrigger_Clicked() {
+    alert("Add trigger clicked");
+}
+
+function btnAddStep_Clicked() {
+    alert("Add step clicked");
+}
