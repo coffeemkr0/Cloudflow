f744d10d91dfe64491db0b064179392751aa3894
coffeemkr0 <mike.e.coffey@gmail.com>
2017-06-24 17:23:41 -0500

    Get the extension browser dialog showing a scrollable area for the extensions in each library by setting a max height on each library's list of extensions.

---
+++ Cloudflow/Cloudflow.Web/Scripts/Shared/ExtensionBrowser.js
@@ -0,0 +1,5 @@
+﻿$(function () {
+    $("#modalExtensionBrowser").on("show.bs.modal", function () {
+        $(".extensionBrowser__extensionList").css('max-height', $(window).height() * 0.6);
+    });
+});
