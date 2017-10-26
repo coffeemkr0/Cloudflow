b4df36fd44300a8f6bf36bd7b6a298b066cfecd4
coffeemkr0 <mike.e.coffey@gmail.com>
2017-10-26 18:21:59 -0500

    #12 Add the Condition class.

---
+++ Cloudflow/Cloudflow.Core/Extensions/Condition.cs
@@ -0,0 +1,30 @@
+﻿using Cloudflow.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloudflow.Core.Extensions
+{
+    public abstract class Condition : ConfigurableExtension
+    {
+        #region Properties
+        public ExtensionConfiguration ConditionConfiguration { get; }
+
+        public log4net.ILog ConditionLogger { get; }
+        #endregion
+
+        #region Constructors
+        public Condition(ExtensionConfiguration conditionConfiguration) : base()
+        {
+            this.ConditionConfiguration = conditionConfiguration;
+            this.ConditionLogger = log4net.LogManager.GetLogger($"Condition.{conditionConfiguration.Name}");
+        }
+        #endregion
+
+        #region Public Methods
+        public abstract bool CheckCondition();
+        #endregion
+    }
+}
