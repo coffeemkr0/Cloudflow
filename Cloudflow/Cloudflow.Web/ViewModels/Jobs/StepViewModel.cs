3d4ddeca743fff48cd715fdb6d74f1be13c932fe
coffeemkr0 <mike.e.coffey@gmail.com>
2017-11-05 08:49:27 -0600

    Add a new view and models for a new job editor.

---
+++ Cloudflow/Cloudflow.Web/ViewModels/Jobs/StepViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cloudflow.Web.ViewModels.Jobs
+{
+    public class StepViewModel : ExtensionConfigurationViewModel
+    {
+        #region Properties
+        public List<ConditionConfigurationViewModel> Conditions { get; set; }
+        #endregion
+
+        #region Constructors
+        public StepViewModel()
+        {
+            this.Conditions = new List<ConditionConfigurationViewModel>();
+        }
+        #endregion
+    }
+}
