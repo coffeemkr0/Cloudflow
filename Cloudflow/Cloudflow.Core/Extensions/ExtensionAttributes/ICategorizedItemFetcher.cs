891881940a16b511489f790d5fd3f51865ea1141
coffeemkr0 <mike.e.coffey@gmail.com>
2017-11-22 09:00:22 -0600

    #13 Add some comments about why I used an interface for loading items into a categorized item collection.

--- Cloudflow/Cloudflow.Core/Extensions/ExtensionAttributes/ICategorizedItemFetcher.cs
+++ Cloudflow/Cloudflow.Core/Extensions/ExtensionAttributes/ICategorizedItemFetcher.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace Cloudflow.Core.Extensions.ExtensionAttributes
 {
+    //Notes:
+    //Why did I use an interface like this?
+    //Sticking purely to attributes to try to load the categorized item colleciton was turning out to be impossible.
+    //Attributes cannot except delegates in their constructors, so it was not possible to specify a method to call in the attribute declaration.
+
+    //I could have used reflection or extensibility in the attribute to callback to a static method on a specified class or plugin, but using static methods made
+    //it unsafe if there were multiple collections on the same class.
+    
+    //I also needed a way for the class with the collection to be able to figure out how it loads the items for a given collection.
+    //For example, I needed the Triggers collection to be able to load only trigger extensions.
+    //There is a clear example of how this is implemented in the EditJobViewModel.
+
     /// <summary>
     /// An interface that a class should implement when the class has a collection property with the CategorizedItemCollectionAttribute attribute specifed.
     /// The interface provieds functionality for loading the items that should be displayed for a categorized item selector.
