a4393004d5c8291d32ed4d28494080a0d32c912e
coffeemkr0 <mike.e.coffey@gmail.com>
2018-04-13 20:39:00 -0500

    Revert "Resharper refactor - fix code quality issues."
    This reverts commit 266a6ed94f21b79e683394be779dd75e40c3b3f7.

--- Cloudflow/Cloudflow.Core/Properties/Resources.Designer.cs
+++ Cloudflow/Cloudflow.Core/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Cloudflow.Core.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,15 +57,6 @@ namespace Cloudflow.Core.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Press Ctrl+C or close this window to stop the agent host..
-        /// </summary>
-        internal static string CloseConsoleWindow_Message {
-            get {
-                return ResourceManager.GetString("CloseConsoleWindow_Message", resourceCulture);
             }
         }
         
