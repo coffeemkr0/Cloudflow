f05974ae5735ea0635dfd270534e6b6b8722a4cf
coffeemkr0 <mike.e.coffey@gmail.com>
2018-04-22 08:27:50 -0500

    Add a StepExtensionService for loading steps from extension libraries.

---
+++ Cloudflow/TempProject/Implementations/StepExtensionService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using TempProject.Interfaces;
+
+namespace TempProject.Implementations
+{
+    public class StepExtensionService : IStepExtensionService
+    {
+        [ImportMany] protected IEnumerable<Lazy<IStep, IExtensionMetaData>> StepExtensions = null;
+
+        public StepExtensionService()
+        {
+            //An aggregate catalog that combines multiple catalogs  
+            var catalog = new AggregateCatalog();
+
+            //Adds all the parts found in the calling assembly
+            catalog.Catalogs.Add(new AssemblyCatalog(Assembly.GetCallingAssembly()));
+
+            //Create the CompositionContainer with the parts in the catalog  
+            var container = new CompositionContainer(catalog);
+
+            //Fill the StepExtensions imports
+            container.ComposeParts(this);
+        }
+
+        public IStep GetStep(Guid stepExtensionId)
+        {
+            foreach (var i in StepExtensions)
+                if (i.Metadata.ExtensionId == stepExtensionId)
+                    return i.Value;
+
+            return null;
+        }
+    }
+}
---
+++ Cloudflow/TempProject/Tests/StepExtensionServiceShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TempProject.Implementations;
+
+namespace TempProject.Tests
+{
+    [TestClass]
+    public class StepExtensionServiceShould
+    {
+        private StepExtensionService _stepExtensionService;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            _stepExtensionService = new StepExtensionService();
+        }
+
+        [TestMethod]
+        public void GetStep()
+        {
+            var step = _stepExtensionService.GetStep(Guid.Empty);
+
+            Assert.IsNull(step);
+        }
+    }
+}
